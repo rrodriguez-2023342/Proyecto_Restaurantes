b18c9adce69fc9979f25c27c7fb71522a1af888f
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-02-14 16:54:04 -0600

    Feat: Se agregaron las ultimas funciones del crud para la entidad reporte

--- Restaurantes/src/models/reportes/reporte.controller.js
+++ Restaurantes/src/models/reportes/reporte.controller.js
@@ -57,3 +57,90 @@ export const getReportes = async (req, res) => {
         })
     }
 }
+
+
+export const getReporteById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const reporte = await Reporte.findById(id).populate('restaurante');
+
+        if (!reporte) {
+            return res.status(404).json({
+                success: false,
+                message: 'Reporte no encontrado'
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Reporte obtenido exitosamente',
+            data: reporte
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al obtener el reporte',
+            error: error.message
+        });
+    }
+}
+
+export const updateReporte = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const reporteData = req.body;
+
+        const reporteEditado = await Reporte.findByIdAndUpdate(
+            id,
+            reporteData,
+            { new: true, runValidators: true }
+        )
+
+        if (!reporteEditado) {
+            return res.status(404).json({
+                success: false,
+                message: 'Reporte no encontrado'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Reporte editado exitosamente',
+            data: reporteEditado
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al editar el reporte',
+            error: error.message
+        });
+    }
+}
+
+export const deleteReporte = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const reporteEliminado = await Reporte.findByIdAndDelete(id);
+
+        if (!reporteEliminado) {
+            return res.status(404).json({
+                success: false,
+                message: 'Reporte no encontrado'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Reporte eliminado correctamente',
+            data: reporteEliminado
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al eliminar el reporte',
+            error: error.message
+        });
+    }
+};
